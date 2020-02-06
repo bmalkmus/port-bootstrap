9c09f6b9ac570796b6a7aeae4aaf603c843fa5b4
bmalkmus <bmalkmus23@gmail.com>
2020-02-05 23:26:44 -0800

    added style

--- Assets/JS/portfolio.js
+++ Assets/JS/portfolio.js
@@ -1,65 +1,65 @@
 
-let proDiv
+$('document').ready(function(){
+
+    let proDiv
 
 
-$(".projects").click(function(){
-    $(proDiv).empty();
-    proDiv = $("<div>");
-    $(proDiv).addClass("popUp");
-    let title = $("<h2>").text(this.p);
-    let screenshot = $('<img>').attr({
-        src: this.dataset.screen,
-        width: '700px',
-        height: '400px'
-    });
-    let github = $('<a>').attr({href: this.dataset.deployed, target: "_blank"});
-    $(github).html('GitHub Deployment')
-    let gitHubP = $('<p>');
-    let repoP = $("<p>");
-    let repo = $('<a>').attr({href: this.dataset.repo, target: "_blank"});
-    $(repo).html('GitHub Repository');
-    let close = $('<span>').html("&times;");
-    $(close).addClass("close");
-    $(proDiv).append(close);
-    $(proDiv).append(title);
-    $(proDiv).append(screenshot);
-    $(gitHubP).append(github);
-    $(repoP).append(repo);
-    $(proDiv).append(repoP);
-    $(proDiv).append(gitHubP);
-    proDiv.css({
-        display: "inline-block",
-        position: 'fixed',
-        zIndex: '1',
-        // paddingTop: '100px',
-        width: '800px',
-        height: '80%',
-        overflow: 'auto',
-        border: 'solid 3px black',
-        backgroundColor: "White",
+    $(".projects").click(function(){
+        $(proDiv).empty();
+        proDiv = $("<div>");
+        $(proDiv).addClass("popUp");
+        let title = $("<h2>").text(this.p);
+        let screenshot = $('<img>').attr({
+            src: this.dataset.screen,
+            width: '700px',
+            height: '400px'
+        });
+        screenshot.css('border', '2px solid #109dac');
+        let github = $('<a>').attr({href: this.dataset.deployed, target: "_blank"});
+        $(github).html('GitHub Deployment')
+        let gitHubP = $('<p>');
+        let repoP = $("<p>");
+        let repo = $('<a>').attr({href: this.dataset.repo, target: "_blank"});
+        $(repo).html('GitHub Repository');
+        let close = $('<span>').html("&times;");
+        $(close).addClass("close");
+        $(proDiv).append(close);
+        $(proDiv).append(title);
+        $(proDiv).append(screenshot);
+        $(gitHubP).append(github);
+        $(repoP).append(repo);
+        $(proDiv).append(repoP);
+        $(proDiv).append(gitHubP);
+        proDiv.css({
+            display: "inline-block",
+            position: 'fixed',
+            zIndex: '1',
+            width: '800px',
+            height: '80%',
+            overflow: 'auto',
+            border: 'solid 5px rgb(1,84,134)',
+            backgroundColor: "#af9879",
+        })
+        $(".context").prepend(proDiv);
+        $('.projects').css("display", "none");
+        $('footer').css("display", "none");
+
+        $(close).click(function() {
+            $(proDiv).css('display', 'none');
+            $('.projects').css('display', 'inline-block');
+            $('footer').css("display", "block");
+        })
     })
-    $(".context").prepend(proDiv);
-    $('.projectRow').attr("display", "none");
-    // $(window).click(function(event) {
-    //     // console.log(event.target.class);
-    //     // console.log($('.popUp'));
-    //     if (this == $(proDiv)) {
-    //         console.log("click")
-    //       $(proDiv).css('display',"none")
-    //     }})
 
-    $(close).click(function() {
-        $(proDiv).css('display', 'none');
+    $('#submitMail').click(function(event){
+        console.log("click");
+            event.preventDefault();
+            let email = "benjamin.malkmus@outlook.com";
+            var subject = $('#subjectName').label + "  " + $('#subjectAdd') ;
+            var emailBody = $('#mailbody').label;
+            window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+
     })
-})
-
-$('#submitMail').click(function(event){
-    console.log("click");
-        event.preventDefault();
-        let email = "benjamin.malkmus@outlook.com";
-        var subject = $('#subjectName').label + "  " + $('#subjectAdd') ;
-        var emailBody = $('#mailbody').label;
-        window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
 
 })
 
